8a901f247a8ba4b9a0e764e9388e7d58ec329a88
wangcanyi <wangcanyi@usoftchina.com>
2021-07-23 15:12:29 +0800

    项目初始化

---
+++ src/main/java/com/usoft/sdk/fee/utils/encry/HmacSHA256Encoder.java
@@ -0,0 +1,9 @@
+package com.usoft.sdk.fee.utils.encry;
+
+public class HmacSHA256Encoder extends HmacEncoder {
+
+	public HmacSHA256Encoder() {
+		super("HmacSHA256");
+	}
+
+}
---
+++ src/test/java/com/usoft/sdk/fee/client/open/DeleteSdkTest.java
@@ -0,0 +1,18 @@
+package com.usoft.sdk.fee.client.open;
+
+import com.usoft.sdk.fee.utils.ProtoBufUtil;
+import org.junit.Test;
+
+/**
+ * 企业sdk测试类
+ *
+ * @author uas
+ * @date 2020/6/19 16:26
+ */
+public class DeleteSdkTest {
+    /**
+     * 测试地址
+     */
+    private DeleteSdk deleteSdk = new DeleteSdk("https://feerest.uuzcc.cn", "J5jL6X5hdc6FpzOYhxPIzqXfFvmE6D8JWYRCBW7kjK4=");
+
+}
